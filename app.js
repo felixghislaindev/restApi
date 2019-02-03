1648b95901f5b7247de575c2f713641d9f55ff00
felixghislaindev <felixghislain@yahoo.com>
2019-02-03 13:35:07 +0000

    setting up
    setting the folder

--- app.js
+++ app.js
@@ -1,65 +1,50 @@
-"use strict";
-// db config
-"use strict";
+'use strict';
 
-const mongoose = require("mongoose");
+// load modules
+const express = require('express');
+const morgan = require('morgan');
 
-mongoose.connect(
-  "mongodb://localhost:27017/qa",
-  { useNewUrlParser: true }
-);
+// variable to enable global error logging
+const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
-const db = mongoose.connection;
-
-db.on("error", err => {
-  console.log("connection error", err);
-});
-
-db.once("open", () => {
-  console.log("connection to dataabse sucessfull");
-}); //ending db open
-
-// importing express
-const express = require("express");
-// import tng body parser
-const jsonParser = require("body-parser").json();
-// requiring morgan
-const logger = require("morgan");
-// requiring router module
-const routes = require("./routes");
-
-//creating the app
+// create the Express app
 const app = express();
 
-app.use(jsonParser);
-// connecting routes
-// use morgan
-app.use(logger("dev"));
-app.use("/questions", routes);
+// setup morgan which gives us http request logging
+app.use(morgan('dev'));
 
-app.get("/", (req, res) => {
-  res.send("welcome to the api");
-});
+// TODO setup your api routes here
 
-// handling error msg
-// cathing 404
-app.use((req, res, next) => {
-  const err = new Error("not found");
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+// setup a friendly greeting for the root route
+app.get('/', (req, res) => {
   res.json({
-    error: {
-      message: err.message
-    }
+    message: 'Welcome to the REST API project!',
   });
 });
 
-// port the app is listenening on
-const port = process.env.PORT || 3000;
+// send 404 if no other route matched
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route Not Found',
+  });
+});
 
-app.listen(port, () => console.log(`App running on port ${port}`));
+// setup a global error handler
+app.use((err, req, res, next) => {
+  if (enableGlobalErrorLogging) {
+    console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+  }
+
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: {},
+  });
+});
+
+// set our port
+app.set('port', process.env.PORT || 5000);
+
+// start listening on our port
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express server is listening on port ${server.address().port}`);
+});
